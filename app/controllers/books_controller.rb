a3dec035326211a8774367d296e229b6863a673c
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-05-08 10:06:18 +0000

    [Add]newアクション、createアクション、indexの内容とレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
-  def new
-  end
-
+  
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
