147ce305beef3ab028061a313d2cd922ff62c8dd
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-05-08 12:04:48 +0000

    [Add]update成功時のフラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_book_path(book.id)  
+    redirect_to show_book_path(book.id), notice: 'Book was successfully updated.'
   end
   
   def destroy
