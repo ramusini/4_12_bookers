ca37c0ae5f7274be9e2fa19b7ad2eeeb9c7509ec
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-05-09 06:39:55 +0000

    [Update]resourcesメソッドの適応とpath修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to show_book_path(@book.id), notice: 'Book was successfully created.'
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
       @books = Book.all
       render :index
@@ -26,9 +26,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to show_book_path(@book.id), notice: 'Book was successfully updated.'
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
-      
       render :edit
     end
   end
